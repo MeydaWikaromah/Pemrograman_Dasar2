4557b949481a34e44125691fce7a35d9d62124f5
MeydaWikaromah <meydaawikaromah@gmail.com>
2022-09-07 19:10:48 +0700

    mengupload tugas harian 1

---
+++ TugasHarian1_Pemdas2_Meyda_Wikaromah_2110131220015.java
@@ -0,0 +1,9 @@
+public class TugasHarian1_Pemdas2_Meyda_Wikaromah_2110131220015{
+    public static void main(String[] args){
+        System.out.println("Nim : 2110131220015");
+        System.out.println("Nama : Meyda Wikaromah");
+        System.out.println("Alamat : Jl. Kapuas No.12 Kabupaten Kapuas");
+        System.out.println("Hobi : Mendengarkan Musik");
+        System.out.println("Mata Kuliah yang dikuasai : Profesi Kependidikan");
+    }
+}
